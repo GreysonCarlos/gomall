397711f14bae8b4c544193215780288aeb102bdd
Greyson <392644099@qq.com>
2024-12-10 20:13:53 +0800

    "feat: 添加接收错误信息逻辑"

--- demo/demo_proto/cmd/client/main.go
+++ demo/demo_proto/cmd/client/main.go
@@ -2,14 +2,16 @@ package main
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"log"
-	"time"
+	// "time"
 
 	"github.com/GreysonCarlos/demo/demo_proto/kitex_gen/pbapi"
 	"github.com/GreysonCarlos/demo/demo_proto/kitex_gen/pbapi/echoservice"
 	"github.com/bytedance/gopkg/cloud/metainfo"
 	"github.com/cloudwego/kitex/client"
+	"github.com/cloudwego/kitex/pkg/kerrors"
 	"github.com/cloudwego/kitex/pkg/transmeta"
 	"github.com/cloudwego/kitex/transport"
 	consul "github.com/kitex-contrib/registry-consul"
@@ -20,7 +22,7 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	c, err := echoservice.NewClient("demo_proto", client.WithResolver(r), client.WithRPCTimeout(time.Second*3), 
+	c, err := echoservice.NewClient("demo_proto", client.WithResolver(r), 
 		client.WithMetaHandler(transmeta.ClientHTTP2Handler),
 		client.WithTransportProtocol(transport.GRPC),
 	)
@@ -29,10 +31,14 @@ func main() {
 	}
 
 	ctx := metainfo.WithPersistentValue(context.Background(), "CLIENT_NAME", "demo_proto_client")
-	resp, err := c.Echo(ctx, &pbapi.Request{Message: "hello"})
+	resp, err := c.Echo(ctx, &pbapi.Request{Message: "error"})
+	var bizErr *kerrors.GRPCBizStatusError
 	if err != nil {
+		ok := errors.As(err, &bizErr)
+		if ok {
+			fmt.Printf("%#v", bizErr)
+		}
 		log.Fatal(err)
 	}
 	fmt.Printf("%v", resp)
-	time.Sleep(time.Second)
 }
