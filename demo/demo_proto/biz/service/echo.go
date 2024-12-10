79c433a44851438077e4de3946f99e464a792c70
Greyson <392644099@qq.com>
2024-12-10 11:17:06 +0800

    "feat: 添加元信息传递功能"

--- demo/demo_proto/biz/service/echo.go
+++ demo/demo_proto/biz/service/echo.go
@@ -2,7 +2,10 @@ package service
 
 import (
 	"context"
+	"fmt"
+
 	pbapi "github.com/GreysonCarlos/demo/demo_proto/kitex_gen/pbapi"
+	"github.com/bytedance/gopkg/cloud/metainfo"
 )
 
 type EchoService struct {
@@ -15,6 +18,7 @@ func NewEchoService(ctx context.Context) *EchoService {
 // Run create note info
 func (s *EchoService) Run(req *pbapi.Request) (resp *pbapi.Response, err error) {
 	// Finish your business logic.
-
+	clientName, ok := metainfo.GetPersistentValue(s.ctx, "CLIENT_NAME")
+	fmt.Println(clientName, ok)
 	return &pbapi.Response{Message: req.Message}, nil
 }
