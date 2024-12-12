4107836441c48b19a4de8b9677ccd8f202f86920
Greyson <392644099@qq.com>
2024-12-12 19:38:24 +0800

    "feat: 根据api.proto和home.proto生成的业务逻辑代码"

---
+++ app/frontend/biz/service/method1.go
@@ -0,0 +1,27 @@
+package service
+
+import (
+	"context"
+
+	home "github.com/GreysonCarlos/projects/Gomall/app/frontend/hertz_gen/frontend/home"
+	"github.com/cloudwego/hertz/pkg/app"
+)
+
+type Method1Service struct {
+	RequestContext *app.RequestContext
+	Context        context.Context
+}
+
+func NewMethod1Service(Context context.Context, RequestContext *app.RequestContext) *Method1Service {
+	return &Method1Service{RequestContext: RequestContext, Context: Context}
+}
+
+func (h *Method1Service) Run(req *home.Empty) (resp *home.Empty, err error) {
+	//defer func() {
+	// hlog.CtxInfof(h.Context, "req = %+v", req)
+	// hlog.CtxInfof(h.Context, "resp = %+v", resp)
+	//}()
+	// todo edit your code
+	
+	return
+}
