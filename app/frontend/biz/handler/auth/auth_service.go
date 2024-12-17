829f9f116645f317ba9671bda797080c00b82308
Greyson <392644099@qq.com>
2024-12-17 17:52:50 +0800

    "chore: 根据注销逻辑更改注册及登录"

--- app/frontend/biz/handler/auth/auth_service.go
+++ app/frontend/biz/handler/auth/auth_service.go
@@ -6,6 +6,7 @@ import (
 	"github.com/GreysonCarlos/projects/Gomall/app/frontend/biz/service"
 	"github.com/GreysonCarlos/projects/Gomall/app/frontend/biz/utils"
 	auth "github.com/GreysonCarlos/projects/Gomall/app/frontend/hertz_gen/frontend/auth"
+	common "github.com/GreysonCarlos/projects/Gomall/app/frontend/hertz_gen/frontend/common"
 	"github.com/cloudwego/hertz/pkg/app"
 	"github.com/cloudwego/hertz/pkg/protocol/consts"
 )
@@ -48,5 +49,26 @@ func Register(ctx context.Context, c *app.RequestContext) {
 		utils.SendErrResponse(ctx, c, consts.StatusOK, err)
 		return
 	}
+	c.Redirect(consts.StatusOK, []byte("/"))
+}
+
+// Logout .
+// @router /auth/logout [POST]
+func Logout(ctx context.Context, c *app.RequestContext) {
+	var err error
+	var req common.Empty
+	err = c.BindAndValidate(&req)
+	if err != nil {
+		utils.SendErrResponse(ctx, c, consts.StatusOK, err)
+		return
+	}
+
+	_, err = service.NewLogoutService(ctx, c).Run(&req)
+
+	if err != nil {
+		utils.SendErrResponse(ctx, c, consts.StatusOK, err)
+		return
+	}
+	// utils.SendSuccessResponse(ctx, c, consts.StatusOK, resp)
 	c.Redirect(consts.StatusOK, []byte("/"))
 }
