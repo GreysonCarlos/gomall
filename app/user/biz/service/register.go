3fe0c41658ed9428871f66ef18bb1c28c57eb682
Greyson <392644099@qq.com>
2024-12-22 13:56:43 +0800

    "feat: 添加完善用户注册业务逻辑"

--- app/user/biz/service/register.go
+++ app/user/biz/service/register.go
@@ -2,7 +2,12 @@ package service
 
 import (
 	"context"
+	"errors"
+
+	"github.com/GreysonCarlos/gomall/app/user/biz/dal/mysql"
+	"github.com/GreysonCarlos/gomall/app/user/model"
 	user "github.com/GreysonCarlos/gomall/rpc_gen/kitex_gen/user"
+	"golang.org/x/crypto/bcrypt"
 )
 
 type RegisterService struct {
@@ -15,6 +20,26 @@ func NewRegisterService(ctx context.Context) *RegisterService {
 // Run create note info
 func (s *RegisterService) Run(req *user.RegisterReq) (resp *user.RegisterResp, err error) {
 	// Finish your business logic.
+	if req.Email == "" || req.Password == "" || req.PasswordConfirm == "" {
+		return nil, errors.New("email or password is empty")
+	}
+	// 密码校验
+	if req.Password != req.PasswordConfirm {
+		return nil, errors.New("Password not match")
+	}
+	// 加密
+	passwordHashed, err := bcrypt.GenerateFromPassword([]byte(req.Password), bcrypt.DefaultCost)
+	if err != nil {
+		return nil, err
+	}
 
-	return
+	newUser := &model.User{
+		Email: req.Email,
+		PasswordHash: string(passwordHashed),
+	}
+	err = model.Create(*mysql.DB, newUser)
+	if err != nil {
+		return nil, err
+	}
+	return &user.RegisterResp{UserId: int32(newUser.ID)}, nil
 }
--- app/user/biz/service/register_test.go
+++ app/user/biz/service/register_test.go
@@ -3,15 +3,27 @@ package service
 import (
 	"context"
 	"testing"
+
+	"github.com/GreysonCarlos/gomall/app/user/biz/dal/mysql"
 	user "github.com/GreysonCarlos/gomall/rpc_gen/kitex_gen/user"
+	"github.com/joho/godotenv"
 )
 
 func TestRegister_Run(t *testing.T) {
+	err := godotenv.Load("../../.env")
+	if err != nil {
+		panic(err)
+	}
+	mysql.Init()
 	ctx := context.Background()
 	s := NewRegisterService(ctx)
 	// init req and assert value
 
-	req := &user.RegisterReq{}
+	req := &user.RegisterReq{
+		Email: "1demo@admin.com",
+		Password: "jadoiajf",
+		PasswordConfirm: "jadoiajf",
+	}
 	resp, err := s.Run(req)
 	t.Logf("err: %v", err)
 	t.Logf("resp: %v", resp)
