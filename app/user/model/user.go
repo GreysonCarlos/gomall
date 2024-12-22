af6d3fa7967ca5286eb01c374d18c169d75fc4aa
Greyson <392644099@qq.com>
2024-12-22 13:55:59 +0800

    "feat: 新建用户数据结构"

---
+++ app/user/model/user.go
@@ -0,0 +1,18 @@
+package model
+
+import "gorm.io/gorm"
+
+type User struct {
+	gorm.Model
+	Email			string `gorm:"uniqueIndex;type:varchar(128) not null"`
+	PasswordHash	string `gorm:"type:varchar(255) not null"` 
+}
+
+func (User) TableName() string {
+	return "user"
+}
+
+func Create(db gorm.DB, user *User) error {
+	return db.Create(user).Error
+}
+
