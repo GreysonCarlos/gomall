f1b67e2e2305c48e197bd0a348650c8862217a8e
Greyson <392644099@qq.com>
2024-12-22 13:58:48 +0800

    "chore: 修改dsn获取方式"

--- app/user/biz/dal/mysql/init.go
+++ app/user/biz/dal/mysql/init.go
@@ -1,7 +1,11 @@
 package mysql
 
 import (
-	"github.com/GreysonCarlos/gomall/app/user/conf"
+	"fmt"
+	"os"
+
+	"github.com/GreysonCarlos/gomall/app/user/model"
+	// "github.com/GreysonCarlos/gomall/app/user/conf"
 
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
@@ -13,13 +17,38 @@ var (
 )
 
 func Init() {
-	DB, err = gorm.Open(mysql.Open(conf.GetConf().MySQL.DSN),
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:3307)/%s?charset=utf8mb4&parseTime=True&loc=Local",
+		os.Getenv("MYSQL_USER"),
+		os.Getenv("MYSQL_PASSWORD"),	
+		os.Getenv("MYSQL_HOST"),
+		os.Getenv("MYSQL_DATABASE"),
+	)
+	DB, err = gorm.Open(mysql.Open(dsn),
 		&gorm.Config{
 			PrepareStmt:            true,
 			SkipDefaultTransaction: true,
 		},
 	)
+	
 	if err != nil {
 		panic(err)
 	}
+	err = DB.AutoMigrate(&model.User{})
+
+	if err != nil {
+		panic(err)
+	}
+
+	type Version struct {
+		Version	string
+	}
+
+	var v Version
+	err = DB.Raw("select version() as version").Scan(&v).Error
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(v)
 }
