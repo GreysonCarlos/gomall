a8783bfa7b73781ed059393722166fe2b318e9dc
Greyson <392644099@qq.com>
2024-12-28 16:33:12 +0800

    "feat: 新增按邮箱查询功能"

--- app/user/model/user.go
+++ app/user/model/user.go
@@ -12,7 +12,12 @@ func (User) TableName() string {
 	return "user"
 }
 
-func Create(db gorm.DB, user *User) error {
+func Create(db *gorm.DB, user *User) error {
 	return db.Create(user).Error
 }
 
+func GetByEmail(db *gorm.DB, email string) (*User, error) {
+	var user User
+	err := db.Where("email = ?", email).First(&user).Error
+	return &user, err
+}
