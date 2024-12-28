390852a3e6f8b1fbeb000aa4eed6d9d52c998502
Greyson <392644099@qq.com>
2024-12-28 16:34:13 +0800

    "feat: 新增完善登入逻辑"

--- app/user/biz/service/login.go
+++ app/user/biz/service/login.go
@@ -2,7 +2,12 @@ package service
 
 import (
 	"context"
+	"errors"
+
+	"github.com/GreysonCarlos/gomall/app/user/biz/dal/mysql"
+	"github.com/GreysonCarlos/gomall/app/user/model"
 	user "github.com/GreysonCarlos/gomall/rpc_gen/kitex_gen/user"
+	"golang.org/x/crypto/bcrypt"
 )
 
 type LoginService struct {
@@ -15,6 +20,23 @@ func NewLoginService(ctx context.Context) *LoginService {
 // Run create note info
 func (s *LoginService) Run(req *user.LoginReq) (resp *user.LoginResp, err error) {
 	// Finish your business logic.
+	if req.Email == "" || req.Password == "" {
+		return nil, errors.New("email or password is empty")
+	}
+	
+	row, err := model.GetByEmail(mysql.DB, req.Email)
+	if err != nil {
+		return nil, err
+	}
 
-	return
+	err = bcrypt.CompareHashAndPassword([]byte(row.PasswordHash), []byte(req.Password))
+	if err != nil {
+		return nil, err
+	}
+
+	resp = &user.LoginResp{
+		UserId: int32(row.ID),
+	}
+	
+	return resp, nil
 }
