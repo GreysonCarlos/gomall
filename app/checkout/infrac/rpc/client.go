59f885c5dcc87cee0ffc840581bf630cf722ba3a
Greyson <392644099@qq.com>
2025-03-20 15:25:15 +0800

    "结算服务"

---
+++ app/checkout/infrac/rpc/client.go
@@ -0,0 +1,93 @@
+package rpc
+
+import (
+	"sync"
+
+	"github.com/GreysonCarlos/gomall/app/checkout/conf"
+	"github.com/GreysonCarlos/gomall/rpc_gen/kitex_gen/cart/cartservice"
+	"github.com/GreysonCarlos/gomall/rpc_gen/kitex_gen/payment/paymentservice"
+	"github.com/GreysonCarlos/gomall/rpc_gen/kitex_gen/product/productcatalogservie"
+	"github.com/cloudwego/kitex/client"
+	"github.com/cloudwego/kitex/pkg/transmeta"
+	"github.com/cloudwego/kitex/pkg/rpcinfo"
+	"github.com/cloudwego/kitex/transport"
+	consul "github.com/kitex-contrib/registry-consul"
+)
+
+var (
+	CartClient	cartservice.Client
+	ProductClient	productcatalogservie.Client
+	PaymentClient	paymentservice.Client
+	once	sync.Once
+)
+
+func InitClient() {
+	once.Do(func() {
+		initCartClient()
+		initProductClient()
+		initPaymentClient()
+	})
+}
+
+func initCartClient() {
+	// 服务发现
+	var opts []client.Option
+	r, err := consul.NewConsulResolver(conf.GetConf().Registry.RegistryAddress[0])
+	if err != nil {
+		panic(err)
+	}
+
+	opts = append(opts, client.WithResolver(r))
+	opts = append(opts, 
+		client.WithClientBasicInfo(&rpcinfo.EndpointBasicInfo{ServiceName: conf.GetConf().Kitex.Service}),
+		client.WithTransportProtocol(transport.GRPC),
+		client.WithMetaHandler(transmeta.ClientHTTP2Handler),
+	)
+
+	CartClient, err = cartservice.NewClient("cart", opts...)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func initProductClient() {
+	// 服务发现
+	var opts []client.Option
+	r, err := consul.NewConsulResolver(conf.GetConf().Registry.RegistryAddress[0])
+	if err != nil {
+		panic(err)
+	}
+
+	opts = append(opts, client.WithResolver(r))
+	opts = append(opts, 
+		client.WithClientBasicInfo(&rpcinfo.EndpointBasicInfo{ServiceName: conf.GetConf().Kitex.Service}),
+		client.WithTransportProtocol(transport.GRPC),
+		client.WithMetaHandler(transmeta.ClientHTTP2Handler),
+	)
+
+	ProductClient, err = productcatalogservie.NewClient("product", opts...)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func initPaymentClient() {
+	// 服务发现
+	var opts []client.Option
+	r, err := consul.NewConsulResolver(conf.GetConf().Registry.RegistryAddress[0])
+	if err != nil {
+		panic(err)
+	}
+
+	opts = append(opts, client.WithResolver(r))
+	opts = append(opts, 
+		client.WithClientBasicInfo(&rpcinfo.EndpointBasicInfo{ServiceName: conf.GetConf().Kitex.Service}),
+		client.WithTransportProtocol(transport.GRPC),
+		client.WithMetaHandler(transmeta.ClientHTTP2Handler),
+	)
+
+	PaymentClient, err = paymentservice.NewClient("payment", opts...)
+	if err != nil {
+		panic(err)
+	}
+}
